ae208222fb68f42602481b121c6c787b52acb35a
Phuong Ngo <phuongngovan2003@gmail.com>
2024-05-21 22:17:07 +0700

    Change bottom bar style to Material

--- src/Navigation/Main/index.tsx
+++ src/Navigation/Main/index.tsx
@@ -5,8 +5,9 @@ import { TransactionsContainer } from "@/Screens/Transactions";
 import { AddTransactionContainer } from "@/Screens/AddTransaction";
 import { WalletsContainer } from "@/Screens/Wallets";
 import { BudgetsContainer } from "@/Screens/Budgets";
+import { createMaterialBottomTabNavigator } from "react-native-paper/react-navigation";
 
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 // @refresh reset
 export const MainNavigator = () => {
@@ -19,40 +20,35 @@ export const MainNavigator = () => {
         name="Home"
         component={HomeContainer}
         options={{
-          tabBarIconStyle: { display: "none" },
-          tabBarLabelPosition: "beside-icon",
+    
         }}
       />
       <Tab.Screen 
         name="Transactions"
         component={TransactionsContainer}
         options={{
-          tabBarIconStyle: { display: "none" },
-          tabBarLabelPosition: "beside-icon",
+
         }}
       />
       <Tab.Screen
         name="AddTransaction"
         component={AddTransactionContainer}
         options={{
-          tabBarIconStyle: { display: "none" },
-          tabBarLabelPosition: "beside-icon",
+ 
         }}
       />
       <Tab.Screen
         name="Wallets"
         component={WalletsContainer}
         options={{
-          tabBarIconStyle: { display: "none" },
-          tabBarLabelPosition: "beside-icon",
+
         }}
       />
       <Tab.Screen
         name="Budgets"
         component={BudgetsContainer}
         options={{
-          tabBarIconStyle: { display: "none" },
-          tabBarLabelPosition: "beside-icon",
+  
         }}
       />
     </Tab.Navigator>
