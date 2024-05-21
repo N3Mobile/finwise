66112e1e8bdf3c5f9b6087456429b95dc5f68d4f
Phuong Ngo <phuongngovan2003@gmail.com>
2024-05-22 00:53:20 +0700

    Update localization

--- src/Localization/keys/index.ts
+++ src/Localization/keys/index.ts
@@ -3,4 +3,10 @@ export enum LocalizationKey {
   HOME = "home",
   START = "start",
   LOADING = "loading",
+
+  // nav-related
+  TRANSACTIONS = "transactions",
+  WALLETS = "wallets",
+  BUDGETS = "budgets",
+  ADD = "add",
 }
