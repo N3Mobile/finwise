f712e69b16a888356e21baa132a756c0f21fa3dc
Phuong Ngo <phuongngovan2003@gmail.com>
2024-05-27 17:47:59 +0700

    Add reusable selection components

---
+++ src/Components/SelectCategory.tsx
@@ -0,0 +1,89 @@
+import React, { Dispatch, FC, SetStateAction } from "react";
+import { ModalLayout } from "./ModalLayout";
+import { ScrollView } from "react-native";
+import { Appbar, List } from "react-native-paper";
+import { LocalizationKey, i18n } from "@/Localization";
+import { useCategoryIcon } from "@/Hooks/icon";
+import { ALL_EXPENSE_CATEGORIES, ALL_INCOME_CATEGORIES } from "@/Config/category";
+
+interface Props {
+    visible: boolean,
+    setVisible: Dispatch<SetStateAction<boolean>>,
+    all: boolean,
+    income: boolean,
+    expense: boolean,
+    setCategory: Dispatch<SetStateAction<string>>
+}
+
+export const SelectCategory: FC<Props> = ({ visible, setVisible, all, income, expense, setCategory }) => {
+
+    function onSelect(category: string) {
+        setCategory(category);
+        setVisible(false);
+    }
+
+    return (
+        <ModalLayout modalVisible={visible} setModalVisible={setVisible}>
+            <Appbar.Header>
+                <Appbar.BackAction onPress={() => setVisible(false)} />
+                <Appbar.Content title={i18n.t(LocalizationKey.SELECT_CATEGORY)} />
+            </Appbar.Header>
+            <ScrollView>
+                { all ?
+                <List.Section>
+                    <List.Subheader>{i18n.t(LocalizationKey.ALL)}</List.Subheader>
+                    <List.Item 
+                        title={i18n.t(LocalizationKey.ALL)}
+                        left={(props) => { 
+                            const [name, icon, color] = useCategoryIcon("all");
+                            return <List.Icon {...props} icon={icon} color={color} />
+                        }}
+                        onPress={() => onSelect("all")}
+                    />
+                </List.Section>
+                : <></>
+                }
+                { income ?
+                <List.Section>
+                    <List.Subheader>{i18n.t(LocalizationKey.INCOME)}</List.Subheader>
+                    {
+                        ALL_INCOME_CATEGORIES.map(type => {
+
+                            const [name, icon, color] = useCategoryIcon(type);
+                            return (
+                                <List.Item
+                                    key={type}
+                                    title={name}
+                                    left={(props) => <List.Icon {...props} icon={icon} color={color} />}
+                                    onPress={() => onSelect(type)}
+                                />
+                            )
+                        })
+                    }
+                </List.Section>
+                : <></>
+                }
+                { expense ?
+                <List.Section>
+                    <List.Subheader>{i18n.t(LocalizationKey.EXPENSE)}</List.Subheader>
+                    {
+                        ALL_EXPENSE_CATEGORIES.map(type => {
+
+                            const [name, icon, color] = useCategoryIcon(type);
+                            return (
+                                <List.Item
+                                    key={type}
+                                    title={name}
+                                    left={(props) => <List.Icon {...props} icon={icon} color={color} />}
+                                    onPress={() => onSelect(type)}
+                                />
+                            )
+                        })
+                    }
+                </List.Section>
+                : <></>
+                }
+            </ScrollView>
+        </ModalLayout>
+    )
+}
--- src/Localization/keys/index.ts
+++ src/Localization/keys/index.ts
@@ -9,4 +9,34 @@ export enum LocalizationKey {
   WALLETS = "wallets",
   BUDGETS = "budgets",
   ADD = "add",
+
+  // wallets
+  CASH = "cash",
+  CARD = "card",
+  EWALLET = "ewallet",
+
+  // categories
+  ALL = "all",
+  BILL = "bill",
+  EDUCATION = "education",
+  ENTERTAINMENT = "entertainment",
+  FAMILY = "family",
+  FOOD = "food",
+  GIFT = "gift",
+  HEALTH = "health",
+  INSURANCE = "insurance",
+  INVESTMENT = "investment",
+  OUTGOING_TRANSFER = "outgoing-transfer",
+  PAY_INTEREST = "pay-interest",
+  SHOPPING = "shopping",
+  TRANSPORTATION = "transportation",
+  COLLECT_INTEREST = "collect-interest",
+  INCOMING_TRANSFER = "incoming-transfer",
+  SALARY = "salary",
+
+  SELECT_WALLET = "select-wallet",
+  SELECT_CATEGORY = "select-category",
+  SELECT_WALLET_TYPE = "select-wallet-type",
+  EXPENSE = "expense",
+  INCOME = "income",
 }
