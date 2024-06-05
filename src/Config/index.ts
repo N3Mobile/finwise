64a70cbf748f8a2fd891e850fd43b248a1408843
Phuong Ngo <phuongngovan2003@gmail.com>
2024-06-05 21:50:53 +0700

    Change backend URL

--- src/Config/index.ts
+++ src/Config/index.ts
@@ -3,5 +3,5 @@ import * as Constants from 'expo-constants';
 export const Config = {
   API_URL: Constants.default.expoConfig?.hostUri
   ? 'http://' + Constants.default.expoConfig.hostUri.split(':').shift()?.concat(':3000') + '/'
-  : 'finwise.n3.com',
+  : 'https://be-mobile-n3.onrender.com',
 };
