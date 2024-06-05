8d00855e0ed0345d1d63488d7750008a2a791822
Phuong Ngo <phuongngovan2003@gmail.com>
2024-06-05 22:38:17 +0700

    Fix delete account button

--- src/Screens/Settings/SettingsContainer.tsx
+++ src/Screens/Settings/SettingsContainer.tsx
@@ -57,6 +57,7 @@ export const SettingsContainer: FC<Props> = ({ navigation }) => {
                 <List.Item
                     title={i18n.t(LocalizationKey.DELETE_ACCOUNT)}
                     left={(props) => <List.Icon {...props} icon="account-remove" />}
+                    onPress={onDeleteAccount}
                 />
             </List.Section>
             <List.Section>
