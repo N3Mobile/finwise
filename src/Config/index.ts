2d372bb3ad1df16c47d06c96f0e4ece3f3d5141c
Phuong Ngo <phuongngovan2003@gmail.com>
2024-06-05 22:31:18 +0700

    Fix API URL

--- src/Config/index.ts
+++ src/Config/index.ts
@@ -1,7 +1,9 @@
 import * as Constants from 'expo-constants';
 
 export const Config = {
-  API_URL: Constants.default.expoConfig?.hostUri
-  ? 'http://' + Constants.default.expoConfig.hostUri.split(':').shift()?.concat(':3000') + '/'
-  : 'https://be-mobile-n3.onrender.com',
+  API_URL: 
+  // Constants.default.expoConfig?.hostUri
+  // ? 'http://' + Constants.default.expoConfig.hostUri.split(':').shift()?.concat(':3000') + '/'
+  // : 
+  'https://be-mobile-n3.onrender.com/',
 };
